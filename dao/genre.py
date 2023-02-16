6ca36fb3372df745aa667ba1e248522b13b0cadd
Kirill Tuchkin <volkirtu@gmail.com>
2023-02-16 20:49:47 +0300

    Homework 18,Сложная версия. добавлена функция create()

--- dao/genre.py
+++ dao/genre.py
@@ -1,4 +1,6 @@
 from dao.models.genre import Genre
+from sqlalchemy import func
+
 
 # DAO для работы с сущностью Genre
 
@@ -13,9 +15,26 @@ class GenreDAO:
         genres_list = self.session.query(Genre).all()
         return genres_list
 
-    def get_one(self, did):
+    def get_one(self, gid):
         """
         загружает данные одного жанра
         """
-        genre = self.session.query(Genre).get(did)
+        genre = self.session.query(Genre).get(gid)
+        return genre
+
+    def get_max_id(self):
+        """
+        получает последний id
+        """
+        max_id = self.session.query(Genre, func.max(Genre.id)).one()
+        return max_id
+
+    def create(self, requested_genre):
+        """
+        создает новый фильм
+        """
+        genre = Genre(**requested_genre)
+        self.session.add(genre)
+        self.session.commit()
+
         return genre
