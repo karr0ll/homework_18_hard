da1328b205327ffaa5545d5f5af9e9a2b66b9f2f
Kirill Tuchkin <volkirtu@gmail.com>
2023-02-16 20:50:18 +0300

    Homework 18,Сложная версия. добавлена функция check_and_add_genres

--- services/genre.py
+++ services/genre.py
@@ -1,5 +1,8 @@
 from dao.genre import GenreDAO
 
+from flask import request
+
+
 # сервисы для работы с сущностью Genre
 
 class GenreService:
@@ -18,6 +21,23 @@ class GenreService:
         """
         return self.dao.get_all()
 
+    def check_and_add_genres(self, data):
+        """
+        проверяет наличие жанра в таблице
+        добавляет новый жанр, если его нет в таблицe
+        """
+        all_genres = self.dao.get_all()
+        requested_genre = data.get("genre")
 
+        all_genres_response = []
+        for item in all_genres:
+            all_genres_response.append(item.name)
+
+        if requested_genre not in all_genres_response:
+            genre_max_id = self.dao.get_max_id()
+            requested_genre.id = genre_max_id[1] + 1,
+            requested_genre.name = requested_genre.get("name")
+
+            return self.dao.create(requested_genre)
 
 
